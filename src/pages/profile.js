9b8c8904660dd737a9f1f5c54235e7fe2c7775d3
Justin <justinsalamon17@gmail.com>
2022-11-26 12:18:42 -0500

    loading user and start of form

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,5 +1,6 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
+import Parse from "parse/dist/parse.min.js";
 
 const pages = ["Products", "Pricing", "Blog"];
 const settings = ["Profile", "Account", "Dashboard", "Logout"];
@@ -8,10 +9,40 @@ function Profile() {
   const location = useLocation();
   //the data here will be an object since an object was
   const data = location.state;
-  console.log(data);
+
+  const [userProfile, setUserProfile] = useState(null);
+
+  useEffect(() => {
+    const getCurrentUser = async function () {
+      try {
+        const currentUser = await Parse.User.current();
+        setUserProfile(currentUser);
+        return true;
+      } catch (error) {
+        // Error can be caused by wrong parameters or lack of Internet connection
+        alert(`Error! ${error.message}`);
+        return false;
+      }
+    };
+    getCurrentUser();
+  }, []);
+
   return (
     <div>
-      <h1>Hello, {data.name}</h1>
+      <h1>Hello, {userProfile.attributes.username}</h1>
+      <form>
+        <label>
+          Username:
+          <input type="text" name="name" />
+        </label>
+        <input type="submit" value="Submit" />
+        <br></br>
+        <label>
+          First Name:
+          <input type="text" name="name" />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   );
 }
