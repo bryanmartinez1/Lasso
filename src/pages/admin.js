4fb9785b54b4845c3422a1e9049bbb29dfd03cad
Justin <justinsalamon17@gmail.com>
2022-11-28 20:05:08 -0500

    Set up routing for admin menu

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -1,28 +1,35 @@
 import { React } from "react";
-import "../styles/home.css";
-import { useNavigate, Navigate } from "react-router-dom";
-import { Sidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
+import "../styles/admin.css";
+import { Link } from "react-router-dom";
+import { Sidebar, Menu, MenuItem } from "react-pro-sidebar";
 import { ProSidebarProvider } from "react-pro-sidebar";
 
 function Admin() {
   return (
     <section>
       <ProSidebarProvider>
-        <Sidebar>
+        <Sidebar className="sidebar">
           <Menu>
-            <SubMenu label="Charts">
-              <MenuItem> Pie charts </MenuItem>
-              <MenuItem> Line charts </MenuItem>
-            </SubMenu>
-            <MenuItem>Documentation</MenuItem>
-            <MenuItem>Calendar</MenuItem>
+            <MenuItem>
+              <Link id="RouterNavLink" to="/">
+                Home
+              </Link>
+            </MenuItem>
+            <MenuItem>
+              <Link id="RouterNavLink" to="/usersList">
+                Users
+              </Link>
+            </MenuItem>
+            <MenuItem>
+              <Link id="RouterNavLink" to="/admin">
+                return
+              </Link>
+            </MenuItem>
           </Menu>
         </Sidebar>
       </ProSidebarProvider>
-      <div align="center">
-        Stuff goes here conditionally based on MenuItem clicks
-      </div>
     </section>
   );
 }
+
 export default Admin;
---
+++ src/pages/usersList.js
@@ -0,0 +1,13 @@
+import { React, useState } from "react";
+import Parse from "parse/dist/parse.min.js";
+import Admin from "./admin";
+
+function UsersList() {
+  return (
+    <section>
+      <Admin /> Users list
+    </section>
+  );
+}
+
+export default UsersList;
