7cd1d504c8bc01039269d25b32927d287d26f08a
Justin <justinsalamon17@gmail.com>
2022-12-09 20:08:23 -0500

    ?

--- src/pages/productDescription.js
+++ src/pages/productDescription.js
@@ -116,6 +116,7 @@ export default function ProductDescription() {
   }
 
   // if an item is sold, remove the bidding box.
+  // need to handle not allowing bidding after end date.
   return (
     <div>
       <ProfileNavbar />
